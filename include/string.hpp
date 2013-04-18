58bd7d0447582bd4938fb65bb66d7604ab6a16f2
Bobby Powers <bobbypowers@gmail.com>
2013-04-18 17:12:23 -0400

    add stubs of array, slice and string

---
+++ include/string.hpp
@@ -0,0 +1,10 @@
+// Copyright 2013 Bobby Powers. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+#pragma once
+#ifndef _ENCIRCLING_STRING_HPP_
+#define _ENCIRCLING_STRING_HPP_
+
+
+#endif // _ENCIRCLING_STRING_HPP_
