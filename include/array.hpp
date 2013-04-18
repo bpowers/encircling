578679449294675abcff7a26424780eff45f0043
Bobby Powers <bobbypowers@gmail.com>
2013-04-18 18:25:46 -0400

    array: initial skeleton implementation

--- include/array.hpp
+++ include/array.hpp
@@ -6,5 +6,53 @@
 #ifndef _ENCIRCLING_ARRAY_HPP_
 #define _ENCIRCLING_ARRAY_HPP_
 
+#include "_common.hpp"
+
+#include <memory>
+#include <cstring>
+
+namespace encircling {
+
+// Without passing an instance of this to std::shared_ptr, Valgrind
+// reports a mismatched free error.
+template <typename T>
+class _ArrayFree
+{
+public:
+    void operator() (T* d) const
+    {
+        delete[] d;
+    }
+};
+
+template <typename T>
+class Array {
+public:
+	typedef T value_type;
+
+	explicit Array(const size_t n) : _n(n), _data(new T[n], _ArrayFree<T>())
+	{
+		memset(_data.get(), 0, n*sizeof(T));
+	}
+	virtual ~Array() {}
+
+	T& operator[](size_t i)
+	{
+		if (unlikely(i >= _n))
+			throw RuntimePanic("Array::operator[] out-of-bounds");
+		return _data.get()[i];
+	}
+	T const& operator[](size_t i) const
+	{
+		if (unlikely(i >= _n))
+			throw RuntimePanic("Array::operator[] out-of-bounds");
+		return _data.get()+i;
+	}
+private:
+	size_t const _n;
+	std::shared_ptr<T> const _data;
+};
+
+} // namespace encircling
 
 #endif // _ENCIRCLING_ARRAY_HPP_
